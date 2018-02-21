6fd7fc73a4ec65817a59cd57d370ad600a812e21
Eric Kramer <eric.kramer@showtime.net>
2018-02-21 17:03:31 -0500

    promises

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const app = require('express')();
 const axios = require('axios');
 const $ = require('cheerio');
 const MongoClient = require('mongodb').MongoClient;
+let mongo;
 app.set('view engine', 'ejs');
 
-MongoClient.connect('mongodb://localhost:27017/moma', (err, db) => {
-  if (err) throw err;
-  app
-    .get('/', (req, res) => {
-      db
-        .collection('Artworks')
-        .aggregate(
+getImage = () => {
+  if (mongo) {
+    console.log('mongo');
+    return new Promise((resolve, reject) => {
+      mongo.then(db => db.collection('Artworks')).then(collection => {
+        let image;
+        collection.aggregate(
           [{ $match: { URL: { $ne: null } } }, { $sample: { size: 1 } }],
           (err, result) => {
+            if (err) reject(new Error('Something went wrong'));
             let src;
             const title = result[0]['Title'];
             const artist = result[0]['Artist'].toString();
@@ -24,12 +26,31 @@ MongoClient.connect('mongodb://localhost:27017/moma', (err, db) => {
               src =
                 'http://moma.org' +
                 $('img.picture__img--work', response.data).attr('src');
-              res.render('index', { title, artist, date, medium, src, thumb });
+              image = { title, artist, date, medium, src, thumb };
+              resolve(image);
             });
           }
         );
-    })
-    .listen(3000, () => {
-      console.log(`app listening on localhost:3000`);
+      });
     });
-});
+  } else {
+    console.log('no mongo');
+    mongo = MongoClient.connect('mongodb://localhost:27017/moma').catch(err =>
+      console.error(err)
+    );
+    getImage();
+  }
+};
+
+app
+  .get('/', async (req, res) => {
+    try {
+      const { title, artist, date, medium, src, thumb } = await getImage();
+      res.render('index', { title, artist, date, medium, src, thumb });
+    } catch (error) {
+      res.render('index', { title: error })
+    }
+  })
+  .listen(3000, () => {
+    console.log(`app listening on localhost:3000`);
+  });
