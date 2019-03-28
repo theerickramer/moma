ef4a4bbe621cdf6b32b409a1d1e41fc62ad6079e
Eric Kramer <theerickramer@gmail.com>
2019-03-28 09:38:08 -0700

    refactors as middleware

--- app.js
+++ app.js
@@ -8,69 +8,70 @@ connectMongo = () => {
   mongo = MongoClient.connect(process.env.MONGODB_URI).catch(err =>
     console.error(err)
   );
-  console.log('mongo connected')
-}
+  console.log('mongo connected');
+};
 
-getImage = () => {
+getImage = (req, res, next) => {
   if (mongo) {
-    return new Promise((resolve, reject) => {
-      mongo
-        .then(db => db.collection('Artworks'))
-        .then(collection => {
-          let image;
-          collection.aggregate(
-            [
-              {
-                $match: {
-                  URL: { $ne: null },
-                  ThumbnailURL: { $exists: true, $ne: null }
-                }
-              },
-              { $sample: { size: 1 } }
-            ],
-            (err, result) => {
-              if (err) reject(new Error(err));
-              console.log('mongo queried');
-              let src;
-              const title = result[0]['Title'];
-              const artist =
-                result[0]['Artist'].length > 1
-                  ? result[0]['Artist'].join(', ')
-                  : result[0]['Artist'];
-              const date = result[0]['Date'];
-              const medium = result[0]['Medium'];
-              const url = result[0]['URL'];
-              const thumb = result[0]['ThumbnailURL'];
-              axios.get(url).then(response => {
-                console.log('moma scraped')
-                src =
-                  'https://moma.org' +
-                  $('img.picture__img--focusable', response.data).attr('src');
-                image = { title, artist, date, medium, src, thumb };
-                resolve(image);
-              });
-            }
-          );
-        });
-    });
-  } else {
-    connectMongo();
-    return getImage();
+    mongo
+      .then(db => db.collection('Artworks'))
+      .then(collection => {
+        let image;
+        collection.aggregate(
+          [
+            {
+              $match: {
+                URL: { $ne: null },
+                ThumbnailURL: { $exists: true, $ne: null }
+              }
+            },
+            { $sample: { size: 1 } }
+          ],
+          (err, result) => {
+            if (err) reject(new Error(err));
+            console.log('mongo queried');
+            const title = result[0]['Title'];
+            const artist =
+              result[0]['Artist'].length > 1
+                ? result[0]['Artist'].join(', ')
+                : result[0]['Artist'];
+            const date = result[0]['Date'];
+            const medium = result[0]['Medium'];
+            const url = result[0]['URL'];
+            const thumb = result[0]['ThumbnailURL'];
+            res.locals.image = { title, artist, date, medium, url, thumb };
+            next();
+          }
+        );
+      });
   }
+};
+
+getHiRes = (req, res, next) => {
+  axios.get(res.locals.image.url).then(response => {
+    console.log('moma scraped');
+    res.locals.image.src =
+      'https://moma.org' +
+      $('img.picture__img--focusable', response.data).attr('src');
+    next();
+  });
+};
+
+sendImage = (req, res, next) => {
+  try {
+    const { title, artist, date, medium, src, thumb } = res.locals.image;
+    res.render('index', { title, artist, date, medium, src, thumb });
+  } catch (error) {
+    res.send(`Something's wrong...`);
+  }
+  next()
 };
 
 connectMongo();
 
 app
   .set('view engine', 'ejs')
-  .get('/', async (req, res) => {
-    try {
-      const { title, artist, date, medium, src, thumb } = await getImage();
-      res.render('index', { title, artist, date, medium, src, thumb });
-    } catch (error) {
-      res.send(`Something's wrong...`);
-    }
-  })
+  .get('/', getImage, getHiRes, sendImage)
   .listen(process.env.PORT || 3000, () => {
     console.log(`app listening on localhost:3000`);
   });
