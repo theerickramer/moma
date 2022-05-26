0e92566cfecff0c48b8dc5eadd12738ec775f9a9
Eric Kramer <theerickramer@gmail.com>
2022-05-26 17:30:21 -0400

    fix vue import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue/dist/vue.js';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
-  el: '#app',
-  template: '<App/>',
+  el: "#app",
+  template: "<App/>",
   components: { App }
 });
